5162ba7405ab9d927c35e73ebc46ac60594be0d7
boc4life <lheandrew@gmail.com>
2018-12-15 22:21:25 -0500

    end saturday, login/logout logic successful?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,39 @@
-$(document).ready(function(){
-
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCMp-r1e-q-6gfuaFNEqfbbh6Tze5DXTTc",
-    authDomain: "rutgersrpsproject.firebaseapp.com",
-    databaseURL: "https://rutgersrpsproject.firebaseio.com",
-    projectId: "rutgersrpsproject",
-    storageBucket: "rutgersrpsproject.appspot.com",
-    messagingSenderId: "367951233429"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCMp-r1e-q-6gfuaFNEqfbbh6Tze5DXTTc",
+  authDomain: "rutgersrpsproject.firebaseapp.com",
+  databaseURL: "https://rutgersrpsproject.firebaseio.com",
+  projectId: "rutgersrpsproject",
+  storageBucket: "rutgersrpsproject.appspot.com",
+  messagingSenderId: "367951233429"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-var loggedIn = false;
+var user = firebase.auth().currentUser;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    $("#welcome").html("Logged in successfully");
+    $(".loginRemove").addClass("d-none");
+    $(".loginAdd").removeClass("d-none");
+    $("#logoutBtn").removeClass("d-none");
+  } else {
+    // No user is signed in.
+    $("#welcome").html("Sign In or Register to Play!");
+    $(".loginRemove").removeClass("d-none");
+    $(".loginAdd").addClass("d-none");
+    $("#logoutBtn").addClass("d-none");
+  }
+});
+
+$(document).ready(function(){
+
+var user = false;
 var signinOpen = false;
 var registerOpen = false;
+var displayName;
 
 $("#signin").on("click", function() {
   if (registerOpen == false) {
@@ -31,5 +49,64 @@ $("#register").on("click", function() {
   }
 })
 
-
+$("#signinBtn").on("click", signIn);
+$("#registerBtn").on("click", register);
+$("#logoutBtn").on("click", logout);
+$("#usernameUpdateBtn").on("click", updateUser);
 })
+
+function signIn() {
+  email = $("#signinEmail").val().trim();
+  password = $("#signinPassword").val().trim();
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == "auth/invalid-email") {
+      alert("Invalid Email");
+    }
+    if (errorCode == "auth/user-not-found") {
+      alert("Username Not Found");
+    }
+    if (errorCode == "auth/wrong-password") {
+      alert("Incorrect Password");
+    } else {
+      alert(errorMessage);
+    }
+  })
+}
+
+function register() {
+  email = $("#registerEmail").val().trim();
+  password = $("#registerPassword").val().trim();
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == "auth/email-already-in-use") {
+      alert("Email already in use");
+    }
+    if (errorCode == "auth/invalid-email") {
+      alert("This does not appear to be a valid email address");
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  })
+}
+
+function updateUser() {
+  var user = firebase.auth().currentUser;
+  newName = $("#usernameUpdate").val().trim();
+  user.updateProfile({
+    displayName: newName
+  }).then(function() {
+    $("#welcome").html("Logged in successfully as " + newName);
+  })
+}
+
+function logout() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
